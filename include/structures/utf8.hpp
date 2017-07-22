34552a211b07d0de50c37b9a6ec969c91451cfbd
Ross Brunton <bruntonross@gmail.com>
2017-07-22 17:39:13 +0100

    More utf8 functions
    Mostly copying functions from the C++ string class

--- include/structures/utf8.hpp
+++ include/structures/utf8.hpp
@@ -8,20 +8,32 @@
 namespace utf8 {
     class Utf8 {
     public:
-        Utf8(const char *string);
-        Utf8(const char *string, bool autofree);
+        Utf8(const char *string, bool autofree=false);
+        Utf8(const Utf8 &copy);
         ~Utf8();
         const char *to_string() const;
 
         uint32_t bytes() const; // Does not include the terminating null
         uint32_t chars() const;
+        Utf8 substr(size_t pos, size_t len) const;
+
+        int compare(Utf8 str) const;
+        int compare(size_t pos, size_t len, Utf8 str) const;
+        int compare(size_t pos, size_t len, Utf8 str, size_t subpos, size_t sublen) const;
+
+        size_t find(Utf8 str, size_t pos = 0) const;
+        size_t find(char c, size_t pos = 0) const;
 
         bool operator==(const Utf8 &other) const;
         Utf8 operator+(const Utf8 &other);
+        char operator[] (int x) const;
+
+        static const size_t npos = -1;
 
     private:
         const char *string;
         bool autofree = false;
+        size_t byte_count = 0;
     };
 }
 
--- src/structures/utf8.cpp
+++ src/structures/utf8.cpp
@@ -5,13 +5,33 @@
 #include "structures/utf8.hpp"
 
 namespace utf8 {
-    Utf8::Utf8(const char *string) {
-        this->string = string;
-    }
-
     Utf8::Utf8(const char *string, bool autofree) {
         this->string = string;
         this->autofree = autofree;
+
+        // Calculate bytes
+        uint32_t p = 0;
+        this->byte_count = UINT32_MAX;
+        while(p < UINT32_MAX) {
+            if(this->string[p] == '\0') {
+                this->byte_count = p;
+                break;
+            }
+            p ++;
+        }
+    }
+
+    Utf8::Utf8(const Utf8 &copy) {
+        uint8_t c;
+        char *buff = (char *)kmalloc(copy.bytes(), 0);
+
+        for(c = 0; copy.string[c] != '\0'; c ++) {
+            buff[c] = copy.string[c];
+        }
+
+        buff[c] = '\0';
+        this->string = buff;
+        this->byte_count = copy.byte_count;
     }
 
     Utf8::~Utf8() {
@@ -25,14 +45,24 @@ namespace utf8 {
     }
 
     uint32_t Utf8::bytes() const {
-        uint32_t p = 0;
-        while(p < UINT32_MAX) {
-            if(this->string[p] == '\0') {
-                return p;
-            }
-            p ++;
+        return this->byte_count;
+    }
+
+    Utf8 Utf8::substr(size_t pos, size_t len) const {
+        size_t c;
+
+        if(len > this->bytes() - pos) {
+            len = this->bytes() - pos;
         }
-        return UINT32_MAX;
+
+        char *newbuff = (char *)kmalloc(len + 1, 0);
+
+        for(c = 0; c < len; c ++) {
+            newbuff[c] = this->string[pos + c];
+        }
+
+        newbuff[len] = '\0';
+        return Utf8(newbuff, true);
     }
 
     uint32_t Utf8::chars() const {
@@ -48,6 +78,52 @@ namespace utf8 {
             p ++;
         }
         return c;
+    }
+
+    int Utf8::compare(size_t pos, size_t len, Utf8 str, size_t subpos, size_t sublen) const {
+        size_t count = len > sublen ? sublen : len;
+
+        for(size_t i = 0; i < count; i ++) {
+            if(this->string[pos + i] != str[subpos + i]) {
+                return this->string[pos + i] - str[subpos + i];
+            }
+        }
+
+        if(len > sublen) {
+            return 1;
+        }else if(sublen < len) {
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+    int Utf8::compare(size_t pos, size_t len, Utf8 str) const {
+        return this->compare(pos, len, str, 0, str.bytes());
+    }
+
+    int Utf8::compare(Utf8 str) const {
+        return this->compare(0, this->bytes(), str, 0, str.bytes());
+    }
+
+    size_t Utf8::find(Utf8 str, size_t pos) const {
+        for(; pos <= this->bytes() - str.bytes(); pos ++) {
+            if(!this->compare(pos, str.bytes(), str)) {
+                return pos;
+            }
+        }
+
+        return Utf8::npos;
+    }
+
+    size_t Utf8::find(char c, size_t pos) const {
+        for(; pos <= this->bytes(); pos ++) {
+            if(this->string[pos] == c) {
+                return pos;
+            }
+        }
+
+        return Utf8::npos;
     }
 
     bool Utf8::operator==(const Utf8 &other) const {
@@ -82,5 +158,9 @@ namespace utf8 {
         newbuff[st] = '\0';
 
         return Utf8(newbuff, true);
+    }
+
+    char Utf8::operator[] (int x) const {
+        return this->string[x];
     }
 }
